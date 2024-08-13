18dabdd9b5777988553fcfb7bcbbc41e08b34488
Dennis Joel <dennisjoelw@gmail.com>
2024-08-13 15:49:41 +0700

    Add Chat List UI component & inputbox
    Created the chat list component with a scrollable area.
    
    Styled the chat list using Tailwind CSS for a clean layout.
    
    Added placeholders and structure for future chat messages.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { OpenAI } from 'openai';
 
-const apiKey = process.env.API_KEY;
+const apiKey = '';
 
 const openai = new OpenAI({ apiKey, dangerouslyAllowBrowser: true });
 
@@ -33,12 +33,37 @@ function Home() {
 
         <div className=' w-1/3  flex flex-col px-6 py-4 text-[14px] text-gray-500 font-medium bg-[#10141e] rounded-xl'>
           <h2 className=' text-[#0084FF] text-[28px] font-semibold mb-4'>SmartChat</h2>
-          <p>Chat List</p>
-          Scrollable chat list here
+
+          <p className=' mb-2'>Chat List</p>
+
+          <div className="overflow-y-auto" style={{ maxHeight: 'calc(100vh - 6rem)' }}>
+
+            <div className="flex flex-col gap-3">
+
+              <div className="p-3 rounded-md bg-[#20232a] cursor-pointer">Pertanyaan Pertama</div>
+              <div className="p-3 rounded-md cursor-pointer">Pertanyaan Kedua</div>
+              <div className="p-3 rounded-md cursor-pointer">Ketiga dan terakhir</div>
+
+            </div>
+            
+          </div>
         </div>
 
-        <div className=' w-2/3  flex flex-col px-4 py-4 bg-[#10141e] rounded-xl'>
-          <h2>Messages n Answer</h2>
+        <div className=' w-2/3  flex flex-col px-4 py-4 bg-[#10141e] rounded-xl h-full justify-end'>
+
+          <div className="flex border border-gray-500 rounded-lg overflow-hidden ">
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Masukkan teks"
+              className="flex-1 py-3 px-3 border-none outline-none bg-[#10141e]"
+            />
+            <button className="bg-blue-500 text-white px-4 py-2 font-semibold hover:bg-blue-600">
+              Submit
+            </button>
+          </div>
+
         </div>
 
       </div>
