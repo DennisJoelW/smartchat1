3036f268cc5a78e0097a8f83c86b2ddec74391f8
DennisJoelW <dennisjoelw@gmail.com>
2024-08-14 16:06:19 +0700

    Implement user login with Laravel API and Axios
    Added a new endpoint in Laravel to fetch user data by username. Integrated this endpoint with Axios in React for user login. Implemented error handling and toast notifications for login feedback.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import send_icon from '../../assets/images/send.png'
 import {GoogleGenerativeAI}  from '@google/generative-ai'
 import TypeIt from "typeit-react";
-import { marked } from 'marked';
+import api from '../../api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { errorToast, successToast } from '../Toast/Toast';
 
+// Gemini Config
 const apiKey = process.env.REACT_APP_GEMINI_API_KEY;
-
 const genAI = new GoogleGenerativeAI(apiKey);
-
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
@@ -16,6 +18,9 @@ const model = genAI.getGenerativeModel({
 function Home() {
   const [input, setInput] = useState('');
   const [chatHistory, setChatHistory] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState('')
+  const [username, setUsername] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,26 +41,70 @@ function Home() {
     } 
   };
 
+  const login  = async () => {
+    setLoading(true);
+
+      if (username.trim() === '') {
+        setLoading(false);
+        errorToast('Username cannot be empty');
+        return;
+      }
+
+      try {
+        const response = await api.get(`/smart-users/username/${username}`);
+        setUser(response.data); 
+        successToast('Succesfully Logged in')
+        console.log(response)
+      } catch (error) {
+        errorToast("User not found")
+      } finally {
+        setLoading(false);
+      }
+
+  }
+
+
   return (
     <div className='flex w-full  bg-[hsl(224,30%,4%)] text-white h-[100vh] py-8 px-8'>
       <div className="w-full px-8 py-4 mx-auto sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl xxl:max-w-xxl flex flex-row font-poppins gap-4 bg-[hsl(224,32%,14%)] rounded-xl">
 
-        <div className=' w-1/3  flex flex-col px-6 py-6 text-[14px] text-gray-500 font-medium bg-[#10141e] rounded-xl'>
+        <div className={` w-1/3  flex flex-col px-6 py-6 text-[14px] text-gray-500 font-medium bg-[#10141e] rounded-xl ${user == ''? "justify-between" : ""}`}>
           <h2 className=' text-[#0084FF] text-[28px] font-semibold mb-6'>SmartChat</h2>
 
-          <p className=' mb-2'>Chat List</p>
 
-          <div className="overflow-y-auto" style={{ maxHeight: 'calc(100vh - 6rem)' }}>
+          {user == '' ? 
+          <div className=' flex flex-col'>
+            <input
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder='Enter your username'
+              className=' px-3 py-3 rounded-xl mb-4 text-white'
+            />
 
-            <div className="flex flex-col gap-3">
-
-              <div className="p-3 rounded-md bg-[#20232a] cursor-pointer">Pertanyaan Pertama</div>
-              <div className="p-3 rounded-md cursor-pointer">Pertanyaan Kedua</div>
-              <div className="p-3 rounded-md cursor-pointer">Ketiga dan terakhir</div>
-
-            </div>
-            
+            <button className=' bg-[#33A5FF] px-3 py-3 rounded-xl text-white hover:bg-[hsl(206,100%,48%)] ease-in-out duration-200' onClick={login}>
+              Login
+            </button>
           </div>
+
+          :           
+          <>
+            <p className=' mb-2'>Chat List</p>
+
+            <div className="overflow-y-auto" style={{ maxHeight: 'calc(100vh - 6rem)' }}>
+
+              <div className="flex flex-col gap-3">
+
+                <div className="p-3 rounded-md bg-[#20232a] cursor-pointer">Pertanyaan Pertama</div>
+                <div className="p-3 rounded-md cursor-pointer">Pertanyaan Kedua</div>
+                <div className="p-3 rounded-md cursor-pointer">Ketiga dan terakhir</div>
+
+              </div>
+              
+            </div>
+          </>}
+
+
         </div>
 
         <div className=' w-2/3 flex flex-col pl-5 py-6 bg-[#10141e] rounded-xl h-full justify-between'>
@@ -102,8 +151,22 @@ function Home() {
           </form>
 
         </div>
-
       </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        limit={2}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
     </div>
   );
 }
---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,28 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+
+export const errorToast = (msg) => {
+  toast.error(msg, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const successToast = (msg) => {
+  toast.success(msg, {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
